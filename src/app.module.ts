73ee0076e89e9c47df46861385b0d5502d3c9c5c
Jamalov <Jamalovn07@gmail.com>
2024-08-17 22:31:30 +0500

    clean up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
-import {  Module } from '@nestjs/common'
+import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from './prisma'
 import { databaseConfig } from './configs'
 import { JwtModule } from '@nestjs/jwt'
 
 @Module({
-	imports: [
-		JwtModule.register({ global: true }),
-		ConfigModule.forRoot({ isGlobal: true, load: [databaseConfig] }),
-		PrismaModule,		
-	],
+	imports: [JwtModule.register({ global: true }), ConfigModule.forRoot({ isGlobal: true, load: [databaseConfig] }), PrismaModule],
 })
 export class AppModule {}
