b8e8d1a3fc3c3e85114e80545881132069183a26
omadbeknasirullayev <omadbeknasirullayev4@gmail.com>
2025-10-22 11:43:02 +0500

    add callback api url

--- src/api/payment/payment.controller.ts
+++ src/api/payment/payment.controller.ts
@@ -1,40 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { PaymentService } from './payment.service';
-import { CreatePaymentDto } from './dto/create-payment.dto';
-import { UpdatePaymentDto } from './dto/update-payment.dto';
-import { LoginDto } from './dto/login.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req } from "@nestjs/common";
+import { PaymentService } from "./payment.service";
+import { CreatePaymentDto } from "./dto/create-payment.dto";
+import { UpdatePaymentDto } from "./dto/update-payment.dto";
+import { LoginDto } from "./dto/login.dto";
 
-@Controller('payment')
+@Controller("payment")
 export class PaymentController {
-  constructor(private readonly paymentService: PaymentService) {}
+	constructor(private readonly paymentService: PaymentService) {}
 
-  @Post('login')
-  login(@Body() dto: LoginDto) {
-    return this.paymentService.login(dto);
-  }
+	@Post("login")
+	login(@Body() dto: LoginDto) {
+		return this.paymentService.login(dto);
+	}
 
-  @Post()
-  create(@Body() createPaymentDto: CreatePaymentDto) {
-    return this.paymentService.create(createPaymentDto);
-  }
+	@Post("callback")
+	callback(@Req() req: Request) {
+		console.log(req);
+		return {
+			status: 1,
+			message: "Успешно",
+		};
+	}
 
-  @Get()
-  findAll() {
-    return this.paymentService.findAll();
-  }
+	@Get()
+	findAll() {
+		return this.paymentService.findAll();
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.paymentService.findOne(+id);
-  }
+	@Get(":id")
+	findOne(@Param("id") id: string) {
+		return this.paymentService.findOne(+id);
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePaymentDto: UpdatePaymentDto) {
-    return this.paymentService.update(+id, updatePaymentDto);
-  }
+	@Patch(":id")
+	update(@Param("id") id: string, @Body() updatePaymentDto: UpdatePaymentDto) {
+		return this.paymentService.update(+id, updatePaymentDto);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.paymentService.remove(+id);
-  }
+	@Delete(":id")
+	remove(@Param("id") id: string) {
+		return this.paymentService.remove(+id);
+	}
 }
