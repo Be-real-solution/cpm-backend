376f53e8c805ceb7c2dd486018ea4c32b0ba1aea
omadbeknasirullayev <omadbeknasirullayev4@gmail.com>
2025-10-30 21:47:11 +0500

    validation add

--- src/api/atmos/dto/confirm-card.dto.ts
+++ src/api/atmos/dto/confirm-card.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
 
 export class ConfirmCardDto {
 	@ApiProperty({ example: 1 })
+	@IsNotEmpty()
 	public transactionId!: number;
 
 	@ApiProperty({ example: "123456" })
+	@IsNotEmpty()
 	public otp!: string;
 }
