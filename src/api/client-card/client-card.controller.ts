0e1201ac5b71b9bf5773298a2ed69b80b58e8f85
omadbeknasirullayev <omadbeknasirullayev4@gmail.com>
2025-11-04 22:07:52 +0500

    Add find client cards by client id

--- src/api/client-card/client-card.controller.ts
+++ src/api/client-card/client-card.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ClientCardService } from './client-card.service';
-import { CreateClientCardDto } from './dto/create-client-card.dto';
-import { UpdateClientCardDto } from './dto/update-client-card.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from "@nestjs/common";
+import { ClientCardService } from "./client-card.service";
+import { CreateClientCardDto } from "./dto/create-client-card.dto";
+import { UpdateClientCardDto } from "./dto/update-client-card.dto";
+import { CurrentLanguage } from "src/common/decorator/current-language";
+import { RolesDecorator } from "../auth/roles/RolesDecorator";
+import { Roles } from "src/common/database/Enums";
+import { JwtAuthGuard } from "../auth/user/AuthGuard";
+import { RolesGuard } from "../auth/roles/RoleGuard";
+import { UseGuards } from "@nestjs/common";
+import { ApiTags, ApiBearerAuth } from "@nestjs/swagger";
 
-@Controller('client-card')
+@ApiTags("Client Cards")
+@Controller("client-card")
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class ClientCardController {
-  constructor(private readonly clientCardService: ClientCardService) {}
+	constructor(private readonly clientCardService: ClientCardService) {}
+
+	@Get("find-all/:clientId")
+  @RolesDecorator(Roles.STORE_ADMIN)
+	findAll(@Param("clientId", ParseUUIDPipe) clientId: string, @CurrentLanguage() lang: string) {
+		return this.clientCardService.findAll(lang, { where: { client: { id: clientId } } });
+	}
 }
